5d134e56c377d506552fb9500198dc71827fb592
jonhartm <jonhartm@umich.edu>
2018-10-27 00:50:10 -0400

    get tweet data from the api and create a list of dicts for info we want

--- get_tweets.py
+++ get_tweets.py
@@ -1,9 +1,21 @@
-import twitter
 import secrets
+import sys, os
+import twitter
 
 api = twitter.Api(consumer_key=secrets.consumer_key,
                   consumer_secret=secrets.consumer_secret,
                   access_token_key=secrets.access_token_key,
                   access_token_secret=secrets.access_token_secret)
 
-print(api.VerifyCredentials())
+def request_tweet_data(ids):
+    tweet_data = []
+    data = api.GetStatuses(ids, trim_user=True)
+    for tweet in data:
+        tweet_data.append({
+                "id":tweet.id,
+                "user":tweet.user.id,
+                "time":tweet.created_at_in_seconds,
+                "text":tweet.text,
+                "urls":",".join([url.expanded_url for url in tweet.urls])
+            })
+    return tweet_data
