14a7bb56b76567ea70fd1557904ea20691893e04
Jules G <juliegil@umich.edu>
2018-11-22 12:29:46 -0500

    sorted through people data

--- people.py
+++ people.py
@@ -38,12 +38,23 @@ with open("C:/Users/julie/Documents/SI650/Individual_People2.txt", 'r') as file:
     info = json.load(file)
 
 #print(info.keys())
-page_soup = BeautifulSoup(info['Bernie Sanders_vt_Senate'], 'html.parser')
-link_items = page_soup.find_all('li')
-for every in link_items:
-    print(every)
-    #name_join = 'Bernie Sanders' + ' ' + "on"
-    #if name_join in every:
-    #    print(every)
-    #for each in every.findChildren('font'):
-    #    print(each.text)
+keep_topics = {}
+
+for each_key in info:
+    page_soup = BeautifulSoup(info[each_key], 'html.parser')
+    link_items = page_soup.find_all('li')
+    info_tabs = ""
+    for every in link_items:
+        new_text = every.text.replace(",", " ")
+        new_text2 = new_text.replace("\r", " ")
+        new_text3 = new_text2.replace("\n", " ")
+        info_tabs = info_tabs + " " + new_text3
+
+    keep_topics[each_key] = info_tabs
+
+#print(keep_topics)
+
+with open("totalsheet.csv", "w", encoding = 'utf-8') as tsw:
+    for each_person in keep_topics:
+        begin = each_person.split("_")
+        tsw.write("{}, {}, {}, {}\n".format(begin[0], begin[1], begin[2], keep_topics[each_person]))
