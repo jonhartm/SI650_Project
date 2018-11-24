57864f40305025f438bad06271824b72f3561019
Jules G <juliegil@umich.edu>
2018-11-24 10:50:49 -0500

    json file of politician stances

--- people.py
+++ people.py
@@ -43,18 +43,19 @@ keep_topics = {}
 for each_key in info:
     page_soup = BeautifulSoup(info[each_key], 'html.parser')
     link_items = page_soup.find_all('li')
-    info_tabs = ""
+    info_tabs = []
     for every in link_items:
-        new_text = every.text.replace(",", " ")
-        new_text2 = new_text.replace("\r", " ")
-        new_text3 = new_text2.replace("\n", " ")
-        info_tabs = info_tabs + " " + new_text3
+        new_text = every.text
+        #new_text2 = new_text.strip()[:-11]
+        # extract ( DATE )
+        #print(new_text2)
+        #new_date = every.text.strip()[-11:]
+        #print(new_date)
+        info_tabs.append(new_text)
 
     keep_topics[each_key] = info_tabs
 
 #print(keep_topics)
 
-with open("totalsheet.csv", "w", encoding = 'utf-8') as tsw:
-    for each_person in keep_topics:
-        begin = each_person.split("_")
-        tsw.write("{}, {}, {}, {}\n".format(begin[0], begin[1], begin[2], keep_topics[each_person]))
+with open("totalsheet.json", "w", encoding = 'utf-8') as tsw:
+    json.dump(keep_topics, tsw)
