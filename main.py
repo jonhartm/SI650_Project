eeabaf60372dda1be69124fb4e1081f8aef850e8
jonhartm <jonhartm@umich.edu>
2018-11-28 01:05:25 -0500

    first step getting flask running

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import argparse
 
-import index
+from flask import Flask, render_template
 
-# create the parser object
-parser = argparse.ArgumentParser(description="Tweet Search program")
+import index
 
 tweet_file = 'output.csv'
 account_file = 'accounts.csv'
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# create the parser object
+parser = argparse.ArgumentParser(
+    description="Tweet Search program",
+    epilog="Run without arguments for Flask application"
+    )
+
+# command line arguments
 parser.add_argument("--createindex", nargs=1, help = "Create an index from the supplied tweet file")
 parser.add_argument("--searchtweets", nargs=1, help = "Search the tweet index")
 parser.add_argument("--searchcombined", nargs=1, help = "Search the combined index")
@@ -20,3 +32,5 @@ elif args.searchtweets != None:
     print(index.search_tweets(args.searchtweets[0]))
 elif args.searchcombined != None:
     print(index.search_combined(args.searchcombined[0]))
+else:
+    app.run(debug=True)
