07de578089db5edf37858f5f00691d3fa9cd5bf1
jonhartm <jonhartm@umich.edu>
2018-11-28 22:03:43 -0500

    output name, twitter link and profile image for each user

--- static/scripts.js
+++ static/scripts.js
@@ -10,7 +10,31 @@ $('#btn_search').click(function() {
     url:'/get_account',
     success: function(response) {
       console.log("ok");
-      console.log(response);
+      // Clear the div og any existing content
+      $(".account_results").empty();
+      // Iterate through the results and add a div for each
+      for (var i = 0; i < response.length; i++) {
+        $(".account_results").append(
+          $("<div>")
+            .addClass("account_info")
+            .append(
+              $("<p>")
+                .addClass("account_user_name")
+                .text(response[i].name)
+            )
+            .append(
+              $("<a>")
+                .addClass("account_twitter_link")
+                .attr("href", 'https://twitter.com/'+response[i].screen_name)
+                .text('@'+response[i].screen_name)
+              )
+            .append(
+              $("<img>")
+                .addClass("account_profile_image")
+                .attr("src", response[i].profile_image)
+            )
+        );
+      }
     },
     error: function(error) {
       console.log("uh oh");
